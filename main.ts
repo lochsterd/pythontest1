331a274fef332d44ad120ed951adc49801c9fda0
Graeme Christie <gchristie@bunnings.com.au>
2022-11-19 16:01:05 +0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+function setupLevel (level: number) {
+    if (level == 1) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    } else {
+    	
+    }
+}
+let currentlevel = 1
 let mySprite = sprites.create(assets.image`tom tv`, SpriteKind.Player)
+setupLevel(currentlevel)
