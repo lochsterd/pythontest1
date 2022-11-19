d4ac2543d4ca5a5ce3aef4386d76ae4800dbe8f2
Graeme Christie <gchristie@bunnings.com.au>
2022-11-19 16:14:19 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+def setupLevel(level: number):
+    if level == 1:
+        tiles.set_current_tilemap(tilemap("""
+            level1
+        """))
+    else:
+        pass
+currentlevel = 1
+mySprite = sprites.create(assets.image("""
+    tom tv
+"""), SpriteKind.player)
+setupLevel(currentlevel)
--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-function setupLevel (level: number) {
+function setupLevel(level: number) {
     if (level == 1) {
-        tiles.setCurrentTilemap(tilemap`level1`)
+        tiles.setCurrentTilemap(tilemap`
+            level1
+        `)
     } else {
-    	
+        
     }
+    
 }
+
 let currentlevel = 1
-let mySprite = sprites.create(assets.image`tom tv`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`
+    tom tv
+`, SpriteKind.Player)
 setupLevel(currentlevel)
